801d2a09f9f62aedbfd4ab939833d247bfa4c450
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2021-10-28 00:37:11 -0400

    Creation Repository

---
+++ PracticeDesignPatternBehavior/PracticeDesignPatternBehavior/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeDesignPatternBehavior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
